2806874fbaa71f1d536a5754e9c667260993f34f
SameerSwankar <89686898+matrolat@users.noreply.github.com>
2023-06-09 17:21:59 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connection =()=>{
-    mongoose.connect("mongodb://localhost:27017/Ezshop",{}).then(
+    mongoose.connect("mongodb+srv://sameerswankar96:sourabh123@ezshop.rcu2wl5.mongodb.net/",{}).then(
         (data)=>{
             console.log(`mongodb connected with server ${data.connection.host}`)
         }
